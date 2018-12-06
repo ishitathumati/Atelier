30e4175e593415104b44aebb2305feb82092c464
Ishita Thumati <ithumati@email.sc.edu>
2018-12-06 18:48:51 -0500

    more side menu changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 //import { LoginPage } from '../pages/login/login';
 import { EditProfPage } from '../pages/edit-prof/edit-prof';
+import { ActivityPage } from '../pages/activity/activity';
+import { SavedPage } from '../pages/saved/saved';
 //import { SettingsPage } from '../pages/settings/settings';
+//import { HelpandSuppPage } from '../pages/helpand-supp/helpand-supp';
+//import { LogOutPage } from '../pages/log-out/log-out';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,7 +19,7 @@ export class MyApp {
   @ViewChild(Nav) nav; Nav;
   rootPage:any = TabsPage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any, icon: string}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen )
    {
@@ -26,7 +30,12 @@ export class MyApp {
       splashScreen.hide();
     });
     this.pages = [
-      {title: 'Edit Profile', component: EditProfPage}
+      {title: 'Edit Profile', component: EditProfPage, icon: 'create'},
+      {title: 'Activity', component: ActivityPage, icon: 'clock'},
+      {title: 'Saved', component: SavedPage, icon: 'archive'}
+      //{title: 'Settings', component: SettingsPage, icon: 'settings'},
+      //{title: 'Help and Support', component: HelpandSuppPage, icon: 'help=circle'},
+      //{title: 'Log Out', component: LogOutPage, icon: 'exit'}
     ];
   }
 openPage(page){
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
-import { ProfilePage } from '../pages/profile/profile';
+//import { ProfilePage } from '../pages/profile/profile';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
