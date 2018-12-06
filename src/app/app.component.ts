f52b15912b6a7c0199f4bcfe5b81cb8b9c07d8f9
Saipriya Sairam <ssairam@email.sc.edu>
2018-12-06 18:35:48 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-//import { ProfilePage } from '../pages/profile/profile';
 import { TabsPage } from '../pages/tabs/tabs';
+
 import { SettingsPage } from '../pages/settings/settings';
 
 @Component({
