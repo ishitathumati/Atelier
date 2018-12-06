5b67cf5c1a46fff8c8376eb9f6ce29357143862a
Ishita Thumati <ithumati@email.sc.edu>
2018-12-06 11:50:28 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { NavController } from 'ionic-angular';
+import { ProfilePage } from '../pages/profile/profile';
 //import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
 
@@ -14,12 +15,19 @@ export class MyApp {
   rootPage:any = LoginPage;
 
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public navCtrl: NavController, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen )
+   {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
   }
+  
+  buttonClicked(){
+    this.navCtrl.push(ProfilePage);
 }
+}
+
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import { ProfilePage } from '../pages/profile/profile';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+
