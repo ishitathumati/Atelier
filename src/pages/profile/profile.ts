8a38d15465f3ef1f8c4c6542c450085597ace6ca
Ishita Thumati <ithumati@email.sc.edu>
2019-02-19 23:29:56 -0500

    .

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 /**
  * Generated class for the ProfilePage page.
@@ -16,7 +16,7 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class ProfilePage { 
   
-  persons: FirebaseListObservable <any>;
+  persons: AngularFireList <any>;
 
 
   constructor(public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
