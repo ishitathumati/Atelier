cf8c5a3b28ef162ff74e348414f5ed96eb370ecf
Meghana Barigala <meghana.barigala@yahoo.com>
2019-04-06 18:46:21 -0400

    explore.ts changes for user search explroe .html changes

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
 import { storage } from 'firebase';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 //import { User } from '../../models/users';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { HomePage } from '../home/home';
 import { AngularFireModule } from 'angularfire2';
-//import { FIREBASE_CONFIG } from '../../app/firebase.config';
-//import { UserUploadsPage } from '../user-uploads/user-uploads';
+import { FIREBASE_CONFIG } from '../../app/firebase.config';
+import { UserUploadsPage } from '../user-uploads/user-uploads';
 import * as firebase from 'firebase';
-//import {storage, initializeApp} from 'firebase';
+import { RequestsProvider } from '../../providers/requests/requests';
+import { UserProvider } from '../../providers/user/user';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { a } from '@angular/core/src/render3';
+import {initializeApp} from 'firebase';
 //import {navCtrl, initializeApp} from firebase;
+import { connreq } from '../../models/request';
 
 /**
  * Generated class for the ExplorePage page.
@@ -22,59 +26,129 @@ import * as firebase from 'firebase';
  //hashtag function
  //search bar accesses uid
  //hash tag bar accesses post id 
+
+
+
+
 @IonicPage()
 @Component({
   selector: 'page-explore',
   templateUrl: 'explore.html',
 })
+
 export class ExplorePage {
 
-  public userslist : Array<any>;
+
+  imageSource1; 
+  imageSource2;
+  imageSource3;
+
+  dbPhoto1;
+  dbPhoto2;
+  dbPhoto3;
+
+  newrequest = {} as connreq;
+  
+  temparr = [];
+  filteredusers = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public userservice: UserProvider, public alertCtrl: AlertController, public requestservice: RequestsProvider) {
+
+
+      this.imageSource1 = 'bluemount';
+      this.imageSource2 = 'dusk';
+      this.imageSource3 = 'scream';
+
+      this.getPhotoURL();
+
+
+      this.userservice.getallusers().then((res: any) => {
+        this.filteredusers = res;
+        this.temparr = res;
+
+      
+
+      
+      })
+  }
+  
+  ionViewDidLoad() 
+  {
+    console.log('ionViewDidLoad ExplorePage');
+  }
+
+  /* sendreq(recipient) {
+    this.newrequest.sender = firebase.auth().currentUser.uid;
+    this.newrequest.recipient = recipient.uid;
+    if (this.newrequest.sender === this.newrequest.recipient)
+      alert('You');
+    else {
+      let successalert = this.alertCtrl.create({
+        title: 'Request sent',
+        subTitle: 'Request sent to ' + recipient.displayName,
+        buttons: ['ok']
+      });
+    
+      this.requestservice.sendrequest(this.newrequest).then((res: any) => {
+        if (res.success) {
+          successalert.present();
+          let sentuser = this.filteredusers.indexOf(recipient);
+          this.filteredusers.splice(sentuser, 1);
+        }
+      }).catch((err) => {
+        alert(err);
+      })
+    }
+  } */
+  
+  searchuser(searchbar) {
+    this.filteredusers = this.temparr;
+    var q = searchbar.target.value;
+    if (q.trim() == '') {
+      return;
+    }
+
+    this.filteredusers = this.filteredusers.filter((v) => {
+      if (v.displayName.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    })
+
+    
+  }
+
+
+
+  
+  getPhotoURL()
+  {
+    firebase.storage().ref().child('explorePics/' + this.imageSource1+ '.png').getDownloadURL().then((url)=>{
+      this.dbPhoto1=url;
+    })
+    firebase.storage().ref().child('explorePics/' + this.imageSource2 + '.png').getDownloadURL().then((url)=>{
+      this.dbPhoto2=url;
+    })
+    firebase.storage().ref().child('explorePics/' + this.imageSource3 + '.png').getDownloadURL().then((url)=>{
+      this.dbPhoto3=url;
+    }) 
+
+  /* public userslist : Array<any>;
   public loadedUserslist: Array<any>;
-  public usersRef: firebase.database.Reference;
+  public usersRef: firebase.database.Reference; */
 
 
 
-    imageSource; 
+/*     imageSource; 
     imageSource2;
     imageSource3;
-    imageSource4;
-    imageSource5;
-    imageSource6;
-    imageSource7;
-    imageSource8;
-    imageSource9;
-    imageSource10;
-    imageSource11;
-    imageSource12;
-    imageSource13;
-    imageSource14;
-    imageSource15;
-    imageSource16;
-    imageSource17;
-    imageSource18;
-    imageSource19;
+    
 
 
     dbPhoto1;
     dbPhoto2;
-    dbPhoto3;
-    dbPhoto4;
-    dbPhoto5;
-    dbPhoto6;
-    dbPhoto7;
-    dbPhoto8;
-    dbPhoto9;
-    dbPhoto10;
-    dbPhoto11;
-    dbPhoto12;
-    dbPhoto13;
-    dbPhoto14;
-    dbPhoto15;
-    dbPhoto16;
-    dbPhoto17;
-    dbPhoto18;
-    dbPhoto19;
+    dbPhoto3; */
+    
   /*
   usersRef: Is for creating a database reference so we can pull the data from Firebase.
   userslist: Is to store the list of user names we’re pulling from Firebase.
@@ -87,54 +161,22 @@ export class ExplorePage {
 
   //private userlist = this.db.list<User>('users-list');
   
-  constructor (public aAuth: AngularFireAuth, public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
-    //initializeApp(FIREBASE_CONFIG);
-    //firebase.initializeApp(config);
-    /*this.imageSource = ['bluemount',
-                       'dusk',
-                       'img1',
-                       'img2',
-                       'img3',
-                       'img4',
-                       'img5',
-                       'img6',
-                       'img7',
-                       'img8',
-                       'img9',
-                       'img10',
-                       'img11',
-                       'img12',
-                       'scream',
-                       'starrynight'];*/
+ /*  constructor (public aAuth: AngularFireAuth, public db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
+  
 
 
       this.imageSource = 'bluemount';
       this.imageSource2 = 'dusk';
-      this.imageSource3 = 'scream';
-      this.imageSource4 = 'starrynight';
-      this.imageSource5 = 'img1';
-      this.imageSource6 = 'img10';
-      this.imageSource7 = 'img11';
-      this.imageSource8 = 'img12';
-      this.imageSource9 = 'img2';
-      this.imageSource10 = 'img3';
-      this.imageSource11 = 'img4';
-      this.imageSource12 = 'img5';
-      this.imageSource13 = 'img6';
-      this.imageSource14= 'img7';
-      this.imageSource15= 'img8';
-      this.imageSource16= 'img9';
-      this.imageSource17= 'rabbi'
-      this.imageSource18= 'learning'
-      this.imageSource19= 'portrait';
+      this.imageSource3 = 'scream'; */
+      
       
 
 
-      this.getPhotoURL();
+  /*     this.getPhotoURL();
     //this.userslist = db.list('/usernames');
 
     //this willl open a reference to our firebase data under the /users node
-    this. usersRef = firebase.database(). ref('/users');
+   this. usersRef = firebase.database(). ref('/users');
     this. usersRef.on('value', userslist=>{
       let users = [];
       userslist.forEach(user=>{
@@ -146,10 +188,10 @@ export class ExplorePage {
     })
 
     
-  }
+  }  */
 
 
-  initializeItems() : void{
+/*   initializeItems() : void{
     this.userslist = this.loadedUserslist;
   }
 
@@ -167,87 +209,28 @@ export class ExplorePage {
         return false;
       }
     })
-  }
+  } */
 
 
 
-  getuserslist() 
+ /*  getuserslist() 
   {
     return this.userslist;
   }
+ */
 
-
-  ionViewDidLoad() 
+  /* ionViewDidLoad() 
   {
     console.log('ionViewDidLoad ExplorePage');
-  }
+  } */
 
-  //getting urls from firebase
-  getPhotoURL()
-  {
-    firebase.storage().ref().child('explorePics/' + this.imageSource+ '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto1=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource2 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto2=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource3 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto3=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource4 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto4=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource5 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto5=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource6 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto6=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto7=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto8=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto9=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto10=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto11=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto12=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto13=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto14=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto15=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto16=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto17=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto18=url;
-    })
-    firebase.storage().ref().child('explorePics/' + this.imageSource7 + '.png').getDownloadURL().then((url)=>{
-      this.dbPhoto19=url;
-    })
+
+   
+
+  
+
     
-
-  }
-
-
-
+  //}
   //HASH TAG STUFF:
   /* Hashtag bar should display random pictures for the user to scroll through
 
@@ -274,3 +257,4 @@ export class ExplorePage {
 
 
 
+}
